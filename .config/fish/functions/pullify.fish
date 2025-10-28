2833851b408f7212575bf1f3c8e54b86bf5531c1
Berkus Decker <berkus@users.noreply.github.com>
2025-10-28 23:08:11 +0200

    Update pullify to support arbitrary git remotes

--- .config/fish/functions/pullify.fish
+++ .config/fish/functions/pullify.fish
@@ -1,3 +1,4 @@
-function pullify
-  git config --add remote.origin.fetch '+refs/pull/*/head:refs/remotes/origin/pr/*'
+function pullify -d "Allow access to GitHub PRs for given remote (origin by default)"
+    set -l remote (string length -q -- $argv[1]; and echo $argv[1]; or echo "origin")
+    git config --add remote.$remote.fetch "+refs/pull/*/head:refs/remotes/$remote/pr/*"
 end
